1cca34b22d5735b28e1ea9908e62c6bbae39963e
DoubleNico <tomescuvlad123@gmail.com>
2025-06-16 19:40:53 +0300

    Added config.js

---
+++ modules/config.js
@@ -0,0 +1,17 @@
+export const DEFAULT_CITY = "București";
+
+export const MOCK_DATA = {
+  name: "București",
+  main: {
+    temp: 22,
+    humidity: 56,
+    pressure: 1012,
+  },
+  weather: [{ description: "cer senin", icon: "01d" }],
+  wind: { speed: 3.6 },
+  visibility: 10000,
+  sys: {
+    sunrise: 1711265400,
+    sunset: 1711312200,
+  },
+};
